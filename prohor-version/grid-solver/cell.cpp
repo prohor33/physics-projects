51a6f594d9613905c4ed72dea5d1ef8243e4faff
Prohor <prohor33@gmail.com>
2013-10-31 00:39:15 +0400

    transfer some old code, should recheck this

--- prohor-version/grid-solver/cell.cpp
+++ prohor-version/grid-solver/cell.cpp
@@ -1,7 +1,7 @@
 #include "cell.h"
 #include "parameters.h"
 
-double Cell::MolMass() {
+double Cell::MolMass() const {
   switch(gas_numb_) {
   case 1:
     return PARAMETERS->mol_mass_gas1_;
@@ -13,21 +13,26 @@ double Cell::MolMass() {
   return 0;
 }
 
-double Cell::H() {
+double Cell::H() const {
+  // TODO: maybe different cell to cell
+  // and it should depend on direction (X, Y, Z)
   return 0.02;
 }
 
 double Cell::P(sep::Axis axis,
-    const int* coord) {
+    vector<int> coord) const {
+  // TODO: why is speed here??
   return PARAMETERS->velocity_[coord[axis]];
 }
 
 double Cell::Limiter(sep::Axis axis,
-    const int* coord) {
+    vector<int> coord) {
+
   if( !neighbor_[axis].next || !neighbor_[axis].prev ) {
     cout << "error limiter" << endl;
     return -1.0f;
   }
+
   double val = speed(coord);
   double next = neighbor_[axis].next->speed(coord);
   double prev = neighbor_[axis].prev->speed(coord);
@@ -39,6 +44,7 @@ double Cell::Limiter(sep::Axis axis,
         sep::min( sep::module(next - val),
         2.0f*sep::module(val - prev)));
     break;
+
   case sep::MC:
     return sep::min(
         sep::min(sep::module(next - prev) / 2.0f,
@@ -46,6 +52,7 @@ double Cell::Limiter(sep::Axis axis,
         2.0f * sep::module(val - prev)) *
         sep::sign(next - val);
     break;
+
   case sep::STRANGE:
     return sep::max((double)0.0f, sep::min(2.0f*sep::module(val - prev),
         sep::min(sep::module(next - val),
@@ -53,6 +60,7 @@ double Cell::Limiter(sep::Axis axis,
         2.0f*sep::module(next - val))))) *
         sep::sign(next - val);
     break;
+
   default:
     cout << "error limiter" << endl;
     return 0.0f;
@@ -61,39 +69,162 @@ double Cell::Limiter(sep::Axis axis,
   return 0;
 }
 
+vector<int> Cell::GetCoord(int index) {
+  // TODO: return real 3d coordinates
+
+  vector<int> coord(3);
+  coord[sep::X] = 13;
+  coord[sep::Y] = 21;
+  coord[sep::Z] = 32;
+
+  return coord;
+}
+
+int Cell::GetIndex(vector<int> coord) {
+  // TODO: to implement
+  return 13;
+}
+
+
 void Cell::ComputeHalfSpeed(double dt, sep::Axis axis) {
+
+  if (type_ == FAKE || type_ == OBTAINED)
+    return;
+
 	if (!neighbor_[axis].next->neighbor_[axis].next ||
 	    !neighbor_[axis].prev->neighbor_[axis].prev) {
 	  // this is border cell, should invoke another function
+	  // TODO: invoke another function
 	  return;
 	}
+
 	double gamma;
 	double p;
-	// TODO: should we replace this block with FOR_EACH?
-	vector<vector<vector<double> > >::iterator cii_x;
-	vector<vector<double> >::iterator cii_xy;
-	vector<double>::iterator cii_xyz;
-	int coord[3];
-	for (cii_x=speed_half_.begin(); cii_x!=speed_half_.end(); ++cii_x) {
-	  for (cii_xy=(*cii_x).begin(); cii_xy!=(*cii_x).end(); ++cii_xy) {
-	    for (cii_xyz=(*cii_xy).begin(); cii_xyz!=(*cii_xy).end(); ++cii_xyz) {
-        coord[sep::X] = cii_x - speed_half_.begin();
-        coord[sep::Y] = cii_xy - (*cii_x).begin();
-        coord[sep::Z] = cii_xyz - (*cii_xy).begin();
-	      p = P(axis, coord);
-	      gamma = dt * p / MolMass() * PARAMETERS->time_step_ / H();
-        if (p > 0.0f) {
-          // for speed > 0
-          (*cii_xyz) = speed(coord) +
-            ( 1.0f - fabs( gamma ) ) / 2.0f * Limiter(axis, coord);
-        }
-        else {
-          // for speed < 0
-          (*cii_xyz) = neighbor_[axis].next->speed(coord) -
-            ( 1.0f - fabs( gamma ) ) / 2.0f *
-            neighbor_[axis].next->Limiter(axis, coord);
-        }
-	    }
+
+	vector<int> coord(3);
+	vector<double>::iterator cii;
+
+	for (cii=speed_half_.begin(); cii!=speed_half_.end(); ++cii) {
+
+	  coord = GetCoord((int)(cii-speed_half_.begin()));
+    p = P(axis, coord);
+    gamma = dt * p / MolMass() * PARAMETERS->time_step_ / H();
+
+    if (p > 0.0f) {
+      // for speed > 0
+
+      (*cii) = speed(coord) +
+        ( 1.0f - fabs( gamma ) ) / 2.0f * Limiter(axis, coord);
+    }
+    else {
+      // for speed < 0
+
+      (*cii) = neighbor_[axis].next->speed(coord) -
+        ( 1.0f - fabs( gamma ) ) / 2.0f *
+        neighbor_[axis].next->Limiter(axis, coord);
+
 	  }
 	}
+}
+
+
+void Cell::ComputeSpeed(double dt, sep::Axis axis) {
+
+  if (type_ == FAKE || type_ == OBTAINED)
+    return;
+
+  if (!neighbor_[axis].next->neighbor_[axis].next ||
+      !neighbor_[axis].prev->neighbor_[axis].prev) {
+    // this is border cell, should invoke another function
+    // TODO: invoke another function
+    return;
+  }
+
+  double gamma;
+  double p;
+
+  vector<int> coord(3);
+  vector<double>::iterator cii;
+
+  for (cii=speed_.begin(); cii!=speed_.end(); ++cii) {
+
+    coord = GetCoord((int)(cii-speed_half_.begin()));
+    p = P(axis, coord);
+    gamma = dt * p / MolMass() * PARAMETERS->time_step_ / H();
+
+    (*cii) = (*cii) -
+        gamma * (speed_half(coord) - neighbor_[axis].prev->speed_half(coord));
+
+  }
+
+}
+
+void Cell::ComputeHalfSpeedPrevIsBorder(double dt, sep::Axis axis) {
+
+  double numenator1 = 0.0f;
+  double numenator2 = 0.0f;
+  double denominator = 0.0f;
+
+  double gamma;
+  double p;
+
+  vector<int> coord(3);
+  vector<double>::iterator cii;
+
+  for (cii=speed_half_.begin(); cii!=speed_half_.end(); ++cii) {
+
+    coord = GetCoord((int)(cii-speed_half_.begin()));
+    p = P(axis, coord);
+    gamma = dt * p / MolMass() * PARAMETERS->time_step_ / H();
+
+    if (p > 0.0f) {
+      // for speed > 0
+
+      denominator += fabs(p / MolMass()) *
+        exp((-1.0f)*p*p/(2.0f*MolMass()*wall_T_));
+    }
+    else {
+      // for speed < 0
+
+      neighbor_[axis].prev->speed(coord) =
+        sep::max((double)0.0, 2.0f*speed(coord) - neighbor_[axis].next->speed(coord));
+
+      neighbor_[axis].prev->speed_half(coord) = speed(coord) -
+        (1.0 - fabs(gamma)) / 2.0f * Limiter(axis, coord);
+
+      numenator1 += fabs(p / MolMass()) *
+          neighbor_[axis].next->speed_half(coord);
+
+      numenator2 += fabs(p / MolMass()) *
+          0.5f * (neighbor_[axis].prev->speed(coord) + speed(coord));
+    }
+
+  }
+
+
+  for (cii=speed_half_.begin(); cii!=speed_half_.end(); ++cii) {
+
+    coord = GetCoord((int)(cii-speed_half_.begin()));
+    p = P(axis, coord);
+    gamma = dt * p / MolMass() * PARAMETERS->time_step_ / H();
+
+    if (p > 0.0f) {
+      // for speed > 0
+
+      wall_T_ =
+         numenator1 / denominator * exp((-1.0f) * p * p / (2.0f * MolMass() * wall_T_));
+
+      MainInfo_->g_speed[x][y][z] =
+        numenator2 / denominator * exp((-1.0f) * p * p / (2.0f * MolMass() * wall_T_));
+
+      neighbor_[axis].prev->speed(coord) =
+        sep::max((double)0.0, 2.0f*MainInfo_->g_speed[x][y][z] -
+          speed(coord));
+
+      speed_half(coord) = speed(coord) +
+        (1.0f - fabs(gamma)) / 2.0f * neighbor_[axis].next->Limiter(axis, coord);
+    }
+
+  }
+
 }
--- prohor-version/grid-solver/cell.h
+++ prohor-version/grid-solver/cell.h
@@ -16,7 +16,7 @@ struct Neighbor {
       prev(prev),
       that(that)
   {};
-
+public:
   Cell *next;
   Cell *prev;
   Cell *that;
@@ -29,8 +29,15 @@ class Cell {
     SECOND = 1
   };
 
-  Cell(GasNumb gas_numb) :
-    gas_numb_(gas_numb)
+  enum CellType {
+    NORMAL = 0,
+    FAKE = 1,
+    OBTAINED = 2
+  };
+
+  Cell(GasNumb gas_numb, CellType type=NORMAL) :
+    gas_numb_(gas_numb),
+    type_(type)
   {};
 
   ~Cell() {};
@@ -38,30 +45,47 @@ class Cell {
   void ComputeHalfSpeed(double dt,
       sep::Axis axis);
 
-  inline double& speed(const int* coord) {
-    return speed_[coord[sep::X]][coord[sep::Y]][coord[sep::Z]];
+  void ComputeSpeed(double dt,
+      sep::Axis axis);
+
+  void ComputeHalfSpeedPrevIsBorder(double dt,
+      sep::Axis axis);
+
+  inline double& speed(vector<int> coord) {
+    return speed_[GetIndex(coord)];
   }
-  inline double& speed_half(const int* coord) {
-    return speed_half_[coord[sep::X]][coord[sep::Y]][coord[sep::Z]];
+  inline double& speed_half(vector<int> coord) {
+    return speed_half_[GetIndex(coord)];
   }
 
-  inline double MolMass();
+  inline double MolMass() const;
 
-  inline double H();
+  inline double H() const;
 
   inline double P(sep::Axis axis,
-      const int* coord);
+      vector<int> coord) const;
 
   inline double Limiter(sep::Axis axis,
-      const int* coord);
+      vector<int> coord);
+
+  inline vector<int> GetCoord(int index);
+
+  int GetIndex(vector<int> coord);
+
+  CellType type() { return type_; };
+
+  double& wall_T() { return wall_T_; };
 
  private:
 
-  vector<vector<vector<double> > > speed_;
-  vector<vector<vector<double> > > speed_half_;
+  vector<double> speed_;
+  vector<double> speed_half_;
 
   vector<Neighbor> neighbor_;  // for x, y, z axis
   GasNumb gas_numb_;
+  CellType type_;
+
+  double wall_T_;
 };
 
 #endif // _CELL_H_
